5844066900357e230c25437ca823ce81e3e9ad0d
Igor Kolomiets <igork@amayasoftware.com>
2015-09-24 18:12:06 -0400

    * make Spring application context a constant

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,15 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final ApplicationContext APPLICATION_CONTEXT = new AnnotationConfigApplicationContext(AppConfig.class);
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        TwitterClient twitterClient = ctx.getBean(TwitterClient.class);
+        TwitterClient twitterClient = APPLICATION_CONTEXT.getBean(TwitterClient.class);
 
         User user = twitterClient.showUserById(146882655);
         logger.debug("Got {}", user);
