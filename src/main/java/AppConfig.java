684a31d47b400de9a114608f10a8ad4685164f39
Igor Kolomiets <igork@amayasoftware.com>
2015-09-24 14:08:11 -0400

    Initial commit

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,41 @@
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+
+@Configuration
+@PropertySource("classpath:twitter.properties")
+public class AppConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public OAuth2RestTemplate restTemplate() {
+        ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
+        details.setId("twitter-client");
+        details.setClientId(env.getProperty("clientID"));
+        details.setClientSecret(env.getProperty("clientSecret"));
+        details.setAccessTokenUri(env.getProperty("accessTokenUri"));
+
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(details);
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+
+        for (HttpMessageConverter<?> httpMessageConverter : restTemplate.getMessageConverters()) {
+            if (httpMessageConverter instanceof MappingJackson2HttpMessageConverter) {
+                ObjectMapper objectMapper = ((MappingJackson2HttpMessageConverter) httpMessageConverter).getObjectMapper();
+                objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            }
+        }
+
+        return restTemplate;
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,154 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+
+    @JsonProperty("id")
+    private long id;
+
+    @JsonProperty("id_str")
+    private String idStr;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("screen_name")
+    private String screenName;
+
+    @JsonProperty("created_at")
+    private String createdAt;
+
+    @JsonProperty("protected")
+    private boolean protectedAccount;
+
+    @JsonProperty("followers_count")
+    private int followersCount;
+
+    @JsonProperty("friends_count")
+    private int friendsCount;
+
+    @JsonProperty("listed_count")
+    private int listedCount;
+
+    @JsonProperty("favourites_count")
+    private int favouritesCount;
+
+    @JsonProperty("verified")
+    private boolean verified;
+
+    @JsonProperty("statuses_count")
+    private int statusesCount;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getIdStr() {
+        return idStr;
+    }
+
+    public void setIdStr(String idStr) {
+        this.idStr = idStr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getScreenName() {
+        return screenName;
+    }
+
+    public void setScreenName(String screenName) {
+        this.screenName = screenName;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isProtectedAccount() {
+        return protectedAccount;
+    }
+
+    public void setProtectedAccount(boolean protectedAccount) {
+        this.protectedAccount = protectedAccount;
+    }
+
+    public int getFollowersCount() {
+        return followersCount;
+    }
+
+    public void setFollowersCount(int followersCount) {
+        this.followersCount = followersCount;
+    }
+
+    public int getFriendsCount() {
+        return friendsCount;
+    }
+
+    public void setFriendsCount(int friendsCount) {
+        this.friendsCount = friendsCount;
+    }
+
+    public int getListedCount() {
+        return listedCount;
+    }
+
+    public void setListedCount(int listedCount) {
+        this.listedCount = listedCount;
+    }
+
+    public int getFavouritesCount() {
+        return favouritesCount;
+    }
+
+    public void setFavouritesCount(int favouritesCount) {
+        this.favouritesCount = favouritesCount;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    public int getStatusesCount() {
+        return statusesCount;
+    }
+
+    public void setStatusesCount(int statusesCount) {
+        this.statusesCount = statusesCount;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", idStr='" + idStr + '\'' +
+                ", name='" + name + '\'' +
+                ", screenName='" + screenName + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                ", protectedAccount=" + protectedAccount +
+                ", followersCount=" + followersCount +
+                ", friendsCount=" + friendsCount +
+                ", listedCount=" + listedCount +
+                ", favouritesCount=" + favouritesCount +
+                ", verified=" + verified +
+                ", statusesCount=" + statusesCount +
+                '}';
+    }
+}
