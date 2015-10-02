23ad668711764b7c536b75b977c108f24f175fcd
Igor Kolomiets <igork@amayasoftware.com>
2015-10-02 14:42:53 -0400

    * properly configure AsyncRestTemplate

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -6,13 +6,19 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.AsyncClientHttpRequest;
 import org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.client.AsyncRestTemplate;
+
+import java.io.IOException;
+import java.net.URI;
 
 @Configuration
 @ComponentScan
@@ -46,8 +52,19 @@ public class AppConfig {
     }
 
     @Bean
-    public AsyncRestTemplate asyncRestTemplate(OAuth2RestTemplate restTemplate) {
-        return new AsyncRestTemplate(new HttpComponentsAsyncClientHttpRequestFactory(), restTemplate);
+    public AsyncRestTemplate asyncRestTemplate(final OAuth2RestTemplate oAuth2RestTemplate) {
+        HttpComponentsAsyncClientHttpRequestFactory asyncRequestFactory = new HttpComponentsAsyncClientHttpRequestFactory() {
+            @Override
+            public AsyncClientHttpRequest createAsyncRequest(URI uri, HttpMethod httpMethod) throws IOException {
+                AsyncClientHttpRequest asyncRequest = super.createAsyncRequest(uri, httpMethod);
+
+                OAuth2AccessToken accessToken = oAuth2RestTemplate.getAccessToken();
+                asyncRequest.getHeaders().set("Authorization", String.format("%s %s", accessToken.getTokenType(), accessToken.getValue()));
+
+                return asyncRequest;
+            }
+        };
+        return new AsyncRestTemplate(asyncRequestFactory, oAuth2RestTemplate);
     }
 
 }
--- src/test/java/TwitterClientTest.java
+++ src/test/java/TwitterClientTest.java
@@ -4,8 +4,12 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.concurrent.CountDownLatch;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -14,17 +18,40 @@ public class TwitterClientTest {
 
     private static final Logger logger = LoggerFactory.getLogger(TwitterClientTest.class);
 
+    private static final long USER_ID = 146882655L;
+
     @Autowired
     private TwitterClient twitterClient;
 
     @Test
     public void testShowUserById() throws Exception {
-        User user = twitterClient.showUserById(146882655);
+        User user = twitterClient.showUserById(USER_ID);
         logger.debug("Got {}", user);
         Assert.assertNotNull(user);
+        Assert.assertEquals(USER_ID, user.getId());
+    }
 
+    @Test
+    public void testShowUserByIdAsync() throws Exception {
+        final CountDownLatch latch = new CountDownLatch(1);
 
-        Assert.assertEquals("https://pbs.twimg.com/profile_images/507133713386598400/7m1JJ2h2_normal.jpeg", user.getProfileImageUrlHttps());
+        twitterClient.showUserByIdAsync(USER_ID, new ListenableFutureCallback<ResponseEntity<User>>() {
+            public void onSuccess(ResponseEntity<User> result) {
+                User user = result.getBody();
+                logger.debug("Got async {}", user);
+                Assert.assertNotNull(user);
+                Assert.assertEquals(USER_ID, user.getId());
+                latch.countDown();
+            }
+
+            public void onFailure(Throwable ex) {
+                logger.error("showUserByIdAsync failed", ex);
+                Assert.fail(ex.getMessage());
+                latch.countDown();
+            }
+        });
+
+        latch.await();
     }
 
 }
